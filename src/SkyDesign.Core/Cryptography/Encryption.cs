e631b9236fedad0d140045650f3320fe8faeb226
Kerem Can <ce.cankerem@outlook.com>
2022-02-15 00:57:46 +0300

    core development

---
+++ src/SkyDesign.Core/Cryptography/Encryption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyDesign.Core.Cryptography
+{
+    public static class Encryption
+    {
+    }
+}
