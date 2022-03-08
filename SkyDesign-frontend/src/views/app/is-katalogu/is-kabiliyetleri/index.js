1b35978d7b89c5e0dc114334a7ae10336bbcef42
tp.mankara <tp.muharrem.ankarali@vakifkatilim.com.tr>
2022-03-08 15:49:07 +0300

    Revert "add frontend"
    This reverts commit a82680da4108185b3d68c90276d6c68ed98d5393.

--- SkyDesign-frontend/src/views/app/is-katalogu/is-kabiliyetleri/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { observer } from "mobx-react-lite";
-
-const IsKabiliyetleri = observer(({ match }) => {
-    return <><p>Is kabiliyetleri</p></>
-});
-
-export default IsKabiliyetleri;
